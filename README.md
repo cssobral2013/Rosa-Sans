3734f50161d8052587ebc2f9f19a92550b7b958f
Cristiano Sobral <cssobral2013@gmail.com>
2023-09-23 21:31:49 -0300

    Update v3.0

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Is developed from Geologica (https://github.com/googlefonts/Geologica)
 
 Main changes include:
 
-- Changed 'Q', '6', and '9' to their Cut alternates;
+- Changed 'Q';
+- Changed '6' and '9' to their Cut alternates;
 - Changed italics to oblique forms;
 - (Variable) Changed to only 1 axis (wght);
+- Changed italic angle to 18 degrees;
 - Some other fixes.
---
+++ generate-woff-woff2.py
@@ -0,0 +1,333 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/RosaSans-Thin.ttf'
+woffPath = './web/RosaSans-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/RosaSans-ThinItalic.ttf'
+woffPath = './web/RosaSans-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/RosaSans-ExtraLight.ttf'
+woffPath = './web/RosaSans-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/RosaSans-ExtraLightItalic.ttf'
+woffPath = './web/RosaSans-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/RosaSans-Light.ttf'
+woffPath = './web/RosaSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-LightItalic.ttf'
+woffPath = './web/RosaSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-LightItalic.ttf'
+woffPath = './web/RosaSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-Regular.ttf'
+woffPath = './web/RosaSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-Italic.ttf'
+woffPath = './web/RosaSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-Medium.ttf'
+woffPath = './web/RosaSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-MediumItalic.ttf'
+woffPath = './web/RosaSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-SemiBold.ttf'
+woffPath = './web/RosaSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-SemiBoldItalic.ttf'
+woffPath = './web/RosaSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-SemiBold.ttf'
+woffPath = './web/RosaSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-SemiBoldItalic.ttf'
+woffPath = './web/RosaSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-Bold.ttf'
+woffPath = './web/RosaSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-BoldItalic.ttf'
+woffPath = './web/RosaSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-ExtraBold.ttf'
+woffPath = './web/RosaSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-ExtraBoldItalic.ttf'
+woffPath = './web/RosaSans-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-Black.ttf'
+woffPath = './web/RosaSans-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RosaSans-BlackItalic.ttf'
+woffPath = './web/RosaSans-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/RosaSans-Thin.ttf'
+woff2Path = './web/RosaSans-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/RosaSans-ThinItalic.ttf'
+woff2Path = './web/RosaSans-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/RosaSans-ExtraLight.ttf'
+woff2Path = './web/RosaSans-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/RosaSans-ExtraLightItalic.ttf'
+woff2Path = './web/RosaSans-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/RosaSans-Light.ttf'
+woff2Path = './web/RosaSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-LightItalic.ttf'
+woff2Path = './web/RosaSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-LightItalic.ttf'
+woff2Path = './web/RosaSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-Regular.ttf'
+woff2Path = './web/RosaSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-Italic.ttf'
+woff2Path = './web/RosaSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-Medium.ttf'
+woff2Path = './web/RosaSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-MediumItalic.ttf'
+woff2Path = './web/RosaSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-SemiBold.ttf'
+woff2Path = './web/RosaSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-SemiBoldItalic.ttf'
+woff2Path = './web/RosaSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-SemiBold.ttf'
+woff2Path = './web/RosaSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-SemiBoldItalic.ttf'
+woff2Path = './web/RosaSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-Bold.ttf'
+woff2Path = './web/RosaSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-BoldItalic.ttf'
+woff2Path = './web/RosaSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-ExtraBold.ttf'
+woff2Path = './web/RosaSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-ExtraBoldItalic.ttf'
+woff2Path = './web/RosaSans-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-Black.ttf'
+woff2Path = './web/RosaSans-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RosaSans-BlackItalic.ttf'
+woff2Path = './web/RosaSans-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
